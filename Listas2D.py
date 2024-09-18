bbad65c79b06c0f44e907f2618d1b10f71bb49af
Santiago GZ <80967269+SantiagoGZ29@users.noreply.github.com>
2024-09-18 19:48:19 -0300

    Add files via upload

--- Listas2D.py
+++ Listas2D.py
@@ -1,6 +1,10 @@
 import numpy as np
 
+lista = [[1,1,1],[2,2,2],[4,4,4]]
+matriz = np.array(lista)
 
-matriz = np.array([[1,1,1],[2,2,2],[4,4,4]])
+print(type(lista))
+print(type(matriz))
+
 print(matriz)
 print("El total de suma de la matriz es de: ",matriz.sum())
